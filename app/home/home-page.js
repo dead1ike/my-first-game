1cb10f6d2273c9ff6fdb41c82ab0da2308211313
Dearo <dearocosmocat@gmail.com>
2021-06-06 14:47:47 +0200

    Update Game

--- app/home/home-page.js
+++
@@ -1,9 +0,0 @@
-import {  } from '@nativescript/core';
-// import TNSPhaser from "@nativescript/canvas-phaser";
-import { HomeViewModel } from './home-view-model'
-
-
-export function onNavigatingTo(args) {
-  const page = args.object
-  page.bindingContext = new HomeViewModel()
-}
--- app/plugin/canvas-phaser.js
+++ app/plugin/canvas-phaser.js
@@ -1,7 +1,5 @@
 import {EventData, Page} from '@nativescript/core';
-import {GestureStateTypes} from "@nativescript/core";
-// import {DemoSharedCanvasPhaser} from '@demo/shared';
-import TNSPhaser from "@nativescript/canvas-phaser";
+import {DemoSharedCanvasPhaser} from '~/games';
 
 export function navigatingTo(args) {
   const page = args.object;
